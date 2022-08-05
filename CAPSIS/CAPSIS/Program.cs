3f3b1c881adcc27aecb6eb2f7abe6e4408f3a574
JohnSReid2 <johnsreid@gmail.com>
2022-08-05 14:25:58 +0100

    refactor: Tidy up inputs and add comments

--- CAPSIS/CAPSIS/Program.cs
+++ CAPSIS/CAPSIS/Program.cs
@@ -4,21 +4,32 @@ using System.Text.RegularExpressions;
 class Software
 {
     public static string cipherText = "start";
+
+    //General Analysis Data
+    public static int characterCount;
+    public static int uniqieCharacters;
     static public void Main()
     {
-        string input = Console.ReadLine();
+        string input = Console.ReadLine(); //Take input from console
 
-        if (input == null) {cipherText = "null";}           
+        if (input == null) {cipherText = "null";}       
         else {cipherText = input;}
 
+        //Remove all spaces and convert to uppercase
         cipherText = String.Concat(cipherText.Where(c => !Char.IsWhiteSpace(c))).ToUpper();
 
         Console.WriteLine("Main Method initiated");
         Console.WriteLine(cipherText);
 
+        //     ====== GENERAL ANALYSIS ======
         GeneralAnalysis general = new GeneralAnalysis(cipherText);
-        Console.WriteLine(general.CharacterCount());
-        Console.WriteLine(general.UniqueCharacters());
+
+        characterCount = general.CharacterCount();
+        uniqieCharacters = general.UniqueCharacters();
+
+        Console.WriteLine("--- General Analysis ---");
+        Console.WriteLine("Character Count = " + characterCount);
+        Console.WriteLine("Unique Character Count = " + uniqieCharacters);
 
 
     }
