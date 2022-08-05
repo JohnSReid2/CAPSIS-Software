eddbaa65174be472a61e32c7abf19a3d0d95ba85
JohnSReid2 <johnsreid@gmail.com>
2022-08-05 14:19:02 +0100

    feat(main): formats input feat(general): Character count and unique character coun

---
+++ CAPSIS/CAPSIS/GeneralAnalysis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class GeneralAnalysis
+{
+	string cipherText;
+
+	public GeneralAnalysis(string mainInput)
+	{
+		cipherText = mainInput;
+	}
+
+
+	public int CharacterCount() 
+	{
+		return cipherText.Length;
+	}
+
+	public int UniqueCharacters()
+    {
+		var count = cipherText.Distinct().Count();
+		return count;
+	}
+}
--- CAPSIS/CAPSIS/Program.cs
+++ CAPSIS/CAPSIS/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Software
+{
+    public static string cipherText = "start";
+    static public void Main()
+    {
+        string input = Console.ReadLine();
+
+        if (input == null) {cipherText = "null";}           
+        else {cipherText = input;}
+
+        cipherText = String.Concat(cipherText.Where(c => !Char.IsWhiteSpace(c))).ToUpper();
+
+        Console.WriteLine("Main Method initiated");
+        Console.WriteLine(cipherText);
+
+        GeneralAnalysis general = new GeneralAnalysis(cipherText);
+        Console.WriteLine(general.CharacterCount());
+        Console.WriteLine(general.UniqueCharacters());
+
+
+    }
+
+}
