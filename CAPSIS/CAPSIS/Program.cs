ce19b2bf861f65bc95154cd12507a4586a9bde78
JohnSReid2 <johnsreid@gmail.com>
2022-08-05 14:33:01 +0100

    refactor(main): Contain analysis output within seperate method

--- CAPSIS/CAPSIS/Program.cs
+++ CAPSIS/CAPSIS/Program.cs
@@ -10,16 +10,19 @@ class Software
     public static int uniqieCharacters;
     static public void Main()
     {
+        Console.WriteLine("Main Method initiated. Enter Cipher Text:");
         string input = Console.ReadLine(); //Take input from console
 
-        if (input == null) {cipherText = "null";}       
+        if (input == null) 
+        {cipherText = "null";}       
         else {cipherText = input;}
 
         //Remove all spaces and convert to uppercase
         cipherText = String.Concat(cipherText.Where(c => !Char.IsWhiteSpace(c))).ToUpper();
 
-        Console.WriteLine("Main Method initiated");
+        Console.WriteLine("Reformatting cipher text...");
         Console.WriteLine(cipherText);
+
 
         //     ====== GENERAL ANALYSIS ======
         GeneralAnalysis general = new GeneralAnalysis(cipherText);
@@ -27,6 +30,11 @@ class Software
         characterCount = general.CharacterCount();
         uniqieCharacters = general.UniqueCharacters();
 
+        OutputAnalysis();
+    }
+
+    static public void OutputAnalysis()
+    {
         Console.WriteLine("--- General Analysis ---");
         Console.WriteLine("Character Count = " + characterCount);
         Console.WriteLine("Unique Character Count = " + uniqieCharacters);
